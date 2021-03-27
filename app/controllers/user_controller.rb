cc24b99753177de5578d08dae3a434a2dd2e9609
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-03-27 23:29:37 +0000

    commit tree d01360ac0ffe69bb5cc6c2764b2e5c79cad217a8

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,24 @@
+class UserController < ApplicationController
+  def new
+  
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- db/migrate/20210327202400_create_tags.rb
+++ db/migrate/20210327202400_create_tags.rb
@@ -1,6 +1,6 @@
-class CreatePosts < ActiveRecord::Migration
+class CreateTags < ActiveRecord::Migration[5.0]
   def change
-    create_table :posts do |t|
+    create_table :tags do |t|
       t.string :name
       t.timestamps null: false
     end
